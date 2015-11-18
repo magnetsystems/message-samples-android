74a75586308a1a2c390243244a7536d11fe7a1e1
Vincent Lau <vincent.lau@magnet.com>
2015-11-18 01:32:37 -0800

    Update to 2.1.0-SNAPSHOT and handle GCM in Soapbox.
    Prepare for pubsub wakeup.

--- SoapBox/app/src/main/java/com/magnet/demo/mmx/soapbox/MyApplication.java
+++ SoapBox/app/src/main/java/com/magnet/demo/mmx/soapbox/MyApplication.java
@@ -1,6 +1,7 @@
 package com.magnet.demo.mmx.soapbox;
 
 import android.app.Application;
+import android.content.Intent;
 
 import com.magnet.max.android.config.MaxAndroidPropertiesConfig;
 import com.magnet.max.android.Max;
@@ -12,8 +13,13 @@ import com.magnet.mmx.client.api.MMXMessage;
  * listener can be registered (if used by the application).
  */
 public class MyApplication extends Application {
+
   public void onCreate() {
     super.onCreate();
+
+    com.magnet.mmx.client.common.Log.setLoggable(null,
+      com.magnet.mmx.client.common.Log.VERBOSE);
+
     //First thing to do is init the Max API.
     Max.init(this.getApplicationContext(),
             new MaxAndroidPropertiesConfig(this, R.raw.magnetmax));
@@ -23,5 +29,14 @@ public class MyApplication extends Application {
         return false;
       }
     });
+
+    // Optionally register a wakeup broadcast intent.  This will be broadcast
+    // when a GCM message is for this MMX application.  If configure properly,
+    // the MMX server will send this GCM to wakeup the device when a message
+    // needs to be delivered.  It is up to the developer to define this intent
+    // and implement/declare the BroadcastReceiver to handle this intent and
+    // thus to call MMXChannel to retrieve pending messages.
+    Intent intent = new Intent("SOAPBOX_WAKEUP");
+    MMX.registerWakeupBroadcast(this, intent);
   }
 }
