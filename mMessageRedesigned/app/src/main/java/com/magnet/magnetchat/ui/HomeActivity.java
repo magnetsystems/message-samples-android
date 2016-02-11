b8d319ac32330dc32447d24f90d19a1ac3c3c062
Alexandra Ditkovskaya <aditkovskaya@GFL-1976.local>
2016-02-11 18:11:58 +0200

    Created home fragment

--- mMessageRedesigned/app/src/main/java/com/magnet/magnetchat/ui/HomeActivity.java
+++ mMessageRedesigned/app/src/main/java/com/magnet/magnetchat/ui/HomeActivity.java
@@ -1,64 +1,31 @@
 package com.magnet.magnetchat.ui;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.magnet.magnetchat.R;
-import com.magnet.magnetchat.core.managers.ChannelCacheManager;
-import com.magnet.magnetchat.helpers.ChannelHelper;
 import com.magnet.magnetchat.helpers.UserHelper;
-import com.magnet.magnetchat.model.Conversation;
-import com.magnet.magnetchat.model.Message;
-import com.magnet.magnetchat.ui.adapters.ConversationsAdapter;
-import com.magnet.magnetchat.ui.custom.CustomSearchView;
-import com.magnet.magnetchat.util.Logger;
 import com.magnet.max.android.ApiError;
 import com.magnet.max.android.User;
-import com.magnet.mmx.client.api.MMX;
-import com.magnet.mmx.client.api.MMXChannel;
-import com.magnet.mmx.client.api.MMXMessage;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class HomeActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemClickListener {
-    private static final String TAG = HomeActivity.class.getSimpleName();
-
-    private AlertDialog leaveDialog;
+public class HomeActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
 
-    private ListView conversationsList;
-    private ProgressBar mProgressBar;
-    private SwipeRefreshLayout swipeContainer;
-
-    private List<Conversation> conversations;
     private String username;
-    private ConversationsAdapter adapter;
 
+    private HomeFragment homeFragment;
 
     @Override
     protected int getLayoutResource() {
@@ -77,8 +44,6 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
         toolbar.setTitle(username);
         setSupportActionBar(toolbar);
 
-        mProgressBar = (ProgressBar) findViewById(R.id.homeProgress);
-
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -91,27 +56,7 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
         TextView navigationName = (TextView) navigationView.getHeaderView(0).findViewById(R.id.homeUserName);
         navigationName.setText(username);
 
-        conversationsList = (ListView) findViewById(R.id.homeConversationsList);
-        conversationsList.setOnItemClickListener(this);
-        conversationsList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                showLeaveDialog(adapter.getItem(position));
-                return true;
-            }
-        });
-        getConversations(true);
-
-        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
-        // Setup refresh listener which triggers new data loading
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                getConversations(false);
-            }
-        });
-        swipeContainer.setColorSchemeResources(R.color.colorPrimaryDark, R.color.colorPrimary, R.color.colorAccent);
-
+        showHomeFragment();
     }
 
     @Override
@@ -127,24 +72,6 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
         toggle.onConfigurationChanged(newConfig);
     }
 
-    private void getConversations(boolean showProgress) {
-        if (showProgress) {
-            mProgressBar.setVisibility(View.VISIBLE);
-        }
-        ChannelHelper.getInstance().readConversations(readChannelInfoListener);
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (adapter != null) {
-            Conversation conversation = adapter.getItem(position);
-            if (conversation != null) {
-                Log.d(TAG, "Channel " + conversation.getChannel().getName() + " is selected");
-                startActivity(ChatActivity.getIntentWithChannel(conversation));
-            }
-        }
-    }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -156,55 +83,12 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-
-            final CustomSearchView search = (CustomSearchView) menu.findItem(R.id.menu_search).getActionView();
-            search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    searchMessage(query);
-                    return true;
-                }
-
-                @Override
-                public boolean onQueryTextChange(String newText) {
-                    if (newText.isEmpty()) {
-                        hideKeyboard();
-                        showAllConversations();
-                    }
-                    return false;
-                }
-            });
-
-            search.setOnCloseListener(new SearchView.OnCloseListener() {
-                @Override
-                public boolean onClose() {
-                    search.onActionViewCollapsed();
-                    showAllConversations();
-                    return true;
-                }
-            });
-        }
-
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         if (toggle.onOptionsItemSelected(item)) {
             return true;
         }
 
-
-        switch (item.getItemId()) {
-            case R.id.menuHomeCreateConversation:
-                startActivity(ChooseUserActivity.getIntentToCreateChannel());
-                break;
-        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -212,8 +96,10 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menuChangePassword:
-                startActivity(new Intent(this, ChangePasswordActivity.class));
+            case R.id.menuHome:
+                showHomeFragment();
+                break;
+            case R.id.menuEvents:
                 break;
             case R.id.menuSignOut:
                 UserHelper.logout(logoutListener);
@@ -224,124 +110,20 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (ChannelCacheManager.getInstance().isConversationListUpdated()) {
-            showAllConversations();
-            ChannelCacheManager.getInstance().resetConversationListUpdated();
-        }
-        MMX.registerListener(eventListener);
-        registerReceiver(onAddedConversation, new IntentFilter(ChannelHelper.ACTION_ADDED_CONVERSATION));
-    }
-
-    @Override
-    protected void onPause() {
-        MMX.unregisterListener(eventListener);
-        unregisterReceiver(onAddedConversation);
-        if (leaveDialog != null && leaveDialog.isShowing()) {
-            leaveDialog.dismiss();
-        }
-        super.onPause();
-    }
-
-    @Override
     public void onClick(View v) {
 
     }
 
-    private void showAllConversations() {
-        showList(ChannelCacheManager.getInstance().getConversations());
+    private void replaceToFragment(Fragment fragment) {
+        replace(fragment, R.id.homeContent);
     }
 
-    private void showList(List<Conversation> conversationsToShow) {
-        if (null == adapter) {
-            conversations = new ArrayList<>(conversationsToShow);
-            adapter = new ConversationsAdapter(this, conversations);
-            conversationsList.setAdapter(adapter);
-        } else {
-            conversations.clear();
-            conversations.addAll(conversationsToShow);
-            adapter.notifyDataSetChanged();
+    private void showHomeFragment() {
+        if (homeFragment == null) {
+            homeFragment = new HomeFragment();
         }
+        replaceToFragment(homeFragment);
     }
-
-    private void updateList() {
-        if (adapter != null) {
-            adapter.notifyDataSetChanged();
-        }
-    }
-
-    private void showLeaveDialog(final Conversation conversation) {
-        if (leaveDialog == null) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setCancelable(false);
-            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    leaveDialog.dismiss();
-                }
-            });
-            leaveDialog = builder.create();
-            leaveDialog.setMessage("Are you sure that you want to leave conversation");
-        }
-        leaveDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Leave", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                mProgressBar.setVisibility(View.VISIBLE);
-                ChannelHelper.getInstance().unsubscribeFromChannel(conversation, new ChannelHelper.OnLeaveChannelListener() {
-                    @Override
-                    public void onSuccess() {
-                        showAllConversations();
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        mProgressBar.setVisibility(View.GONE);
-                        showMessage("Can't leave the conversation");
-                    }
-                });
-                leaveDialog.dismiss();
-            }
-        });
-        leaveDialog.show();
-    }
-
-    private void searchMessage(final String query) {
-        final List<Conversation> searchResult = new ArrayList<>();
-        for (Conversation conversation : ChannelCacheManager.getInstance().getConversations()) {
-            for (Message message : conversation.getMessages()) {
-                if (message.getText() != null && message.getText().toLowerCase().contains(query.toLowerCase())) {
-                    searchResult.add(conversation);
-                    break;
-                }
-            }
-        }
-        showList(searchResult);
-    }
-
-    private ChannelHelper.OnReadChannelInfoListener readChannelInfoListener = new ChannelHelper.OnReadChannelInfoListener() {
-        @Override
-        public void onSuccessFinish(Conversation lastConversation) {
-            finishGetChannels();
-
-            if (null != lastConversation) {
-                showAllConversations();
-            } else {
-                Log.w(TAG, "No conversation is available");
-                showMessage("No conversation is available");
-            }
-        }
-
-        @Override
-        public void onFailure(Throwable throwable) {
-            finishGetChannels();
-        }
-
-        private void finishGetChannels() {
-            swipeContainer.setRefreshing(false);
-            mProgressBar.setVisibility(View.GONE);
-        }
-    };
 
     private UserHelper.OnLogoutListener logoutListener = new UserHelper.OnLogoutListener() {
         @Override
@@ -359,50 +141,6 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
         private void goToHomeActivity() {
             startActivity(new Intent(HomeActivity.this, LoginActivity.class));
             finish();
-        }
-    };
-
-    private MMX.EventListener eventListener = new MMX.EventListener() {
-        @Override
-        public boolean onMessageReceived(MMXMessage mmxMessage) {
-            Logger.debug(TAG, "onMessageReceived");
-            showAllConversations();
-            return false;
-        }
-
-        @Override
-        public boolean onMessageAcknowledgementReceived(User from, String messageId) {
-            Logger.debug(TAG, "onMessageAcknowledgementReceived");
-            updateList();
-            return false;
-        }
-
-        @Override
-        public boolean onInviteReceived(MMXChannel.MMXInvite invite) {
-            Logger.debug(TAG, "onInviteReceived");
-            updateList();
-            return false;
-        }
-
-        @Override
-        public boolean onInviteResponseReceived(MMXChannel.MMXInviteResponse inviteResponse) {
-            Logger.debug(TAG, "onInviteResponseReceived");
-            updateList();
-            return false;
-        }
-
-        @Override
-        public boolean onMessageSendError(String messageId, MMXMessage.FailureCode code, String text) {
-            Logger.debug("onMessageSendError");
-            updateList();
-            return false;
-        }
-    };
-
-    private BroadcastReceiver onAddedConversation = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            showAllConversations();
         }
     };
 
