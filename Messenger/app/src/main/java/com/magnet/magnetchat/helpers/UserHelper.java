924985e48bcd86721edb54003cecf1e8a0957051
Jim Liu <jim.liu@magnet.com>
2016-02-12 22:55:16 -0800

    minor

--- Messenger/app/src/main/java/com/magnet/magnetchat/helpers/UserHelper.java
+++ Messenger/app/src/main/java/com/magnet/magnetchat/helpers/UserHelper.java
@@ -20,7 +20,7 @@ public class UserHelper {
 
     private static final String EMAIL_TEMPLATE = "^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$";
 
-    private static final int MAX_USER_NAMES_LENGTH = 42;
+    private static final int MAX_USER_NAMES_LENGTH = 36;
 
     public static final String MAGNET_SUPPORT_TAG = "magnetsupport";
 
