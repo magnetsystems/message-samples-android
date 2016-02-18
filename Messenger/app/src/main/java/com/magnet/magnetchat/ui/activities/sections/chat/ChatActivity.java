ed38df81238e2ea54911074993cd25d9980d14e0
Jim Liu <jim.liu@magnet.com>
2016-02-18 15:50:03 -0800

    MAX-115 : fixed. refresh subscribers from server

--- Messenger/app/src/main/java/com/magnet/magnetchat/ui/activities/sections/chat/ChatActivity.java
+++ Messenger/app/src/main/java/com/magnet/magnetchat/ui/activities/sections/chat/ChatActivity.java
@@ -205,8 +205,7 @@ public class ChatActivity extends BaseActivity implements GoogleApiClient.Connec
         switch (item.getItemId()) {
             case R.id.menuChatOpenDetails:
                 if (currentConversation != null) {
-                    String name = currentConversation.getChannel().getName();
-                    startActivity(DetailsActivity.createIntentForChannel(name));
+                    startActivity(DetailsActivity.createIntentForChannel(currentConversation));
                 }
                 break;
             case android.R.id.home:
--- Messenger/app/src/main/java/com/magnet/magnetchat/ui/activities/sections/chat/DetailsActivity.java
+++ Messenger/app/src/main/java/com/magnet/magnetchat/ui/activities/sections/chat/DetailsActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import android.widget.ProgressBar;
 import com.magnet.magnetchat.R;
 import com.magnet.magnetchat.core.application.CurrentApplication;
 import com.magnet.magnetchat.core.managers.ChannelCacheManager;
@@ -15,18 +16,26 @@ import com.magnet.magnetchat.model.Conversation;
 import com.magnet.magnetchat.ui.activities.abs.BaseActivity;
 import com.magnet.magnetchat.ui.adapters.UsersAdapter;
 import com.magnet.max.android.User;
+import com.magnet.max.android.UserProfile;
 import com.magnet.max.android.util.StringUtil;
 
 import butterknife.InjectView;
+import com.magnet.mmx.client.api.ListResult;
+import com.magnet.mmx.client.api.MMXChannel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailsActivity extends BaseActivity {
 
-    public static final String TAG_CHANNEL_NAME = "channelName";
+    public static final String TAG_CHANNEL = "channel";
 
     @InjectView(R.id.detailsSubscribersList)
     RecyclerView listView;
 
-    private String channelName;
+    @InjectView(R.id.detailsProgress)
+    ProgressBar detailsProgress;
+
+    private MMXChannel currentChannel;
 
     @Override
     protected int getLayoutResource() {
@@ -44,27 +53,42 @@ public class DetailsActivity extends BaseActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        setTitle("Details");
+
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         layoutManager.setReverseLayout(false);
         listView.setLayoutManager(layoutManager);
 
-        channelName = getIntent().getStringExtra(TAG_CHANNEL_NAME);
-        if (channelName != null) {
-            Conversation currentConversation = ChannelCacheManager.getInstance().getConversationByName(channelName);
-            if(null != currentConversation && null != currentConversation.getChannel()) {
-                UsersAdapter adapter;
-                if (StringUtil.isStringValueEqual(currentConversation.ownerId(), User.getCurrentUserId())) {
-                    adapter = new UsersAdapter(this, currentConversation.getSuppliersList(), addUserListener);
-                } else {
-                    adapter = new UsersAdapter(this, currentConversation.getSuppliersList(), null);
+        currentChannel = getIntent().getParcelableExtra(TAG_CHANNEL);
+        if (currentChannel != null) {
+            detailsProgress.setVisibility(View.VISIBLE);
+            currentChannel.getAllSubscribers(100, 0, new MMXChannel.OnFinishedListener<ListResult<User>>() {
+                @Override public void onSuccess(ListResult<User> userListResult) {
+                    onComplete();
+
+                    List<UserProfile> userProfiles = new ArrayList<>(userListResult.items.size());
+                    for(User u : userListResult.items) {
+                        if(!u.getUserIdentifier().equals(User.getCurrentUserId())) {
+                            userProfiles.add(u);
+                        }
+                    }
+                    bindAdapter(userProfiles);
                 }
-                listView.setAdapter(adapter);
-                setTitle("Details");
-            } else {
-                showMessage("Couldn't load channel. Please try later");
-                finish();
-            }
+
+                @Override
+                public void onFailure(MMXChannel.FailureCode failureCode, Throwable throwable) {
+                    onComplete();
+
+                    exit(failureCode.toString());
+                }
+
+                private void onComplete() {
+                    detailsProgress.setVisibility(View.GONE);
+                }
+            });
+        } else {
+            exit("Channel not set");
         }
     }
 
@@ -88,15 +112,25 @@ public class DetailsActivity extends BaseActivity {
     private UsersAdapter.AddUserListener addUserListener = new UsersAdapter.AddUserListener() {
         @Override
         public void addUser() {
-            startActivity(ChooseUserActivity.getIntentToAddUserToChannel(channelName));
+            startActivity(ChooseUserActivity.getIntentToAddUserToChannel(currentChannel.getName()));
             finish();
         }
     };
 
-    public static Intent createIntentForChannel(String channelName) {
+    public static Intent createIntentForChannel(Conversation conversation) {
         Intent intent = new Intent(CurrentApplication.getInstance(), DetailsActivity.class);
-        intent.putExtra(TAG_CHANNEL_NAME, channelName);
+        intent.putExtra(TAG_CHANNEL, conversation.getChannel());
         return intent;
+    }
+
+    private void bindAdapter(List<UserProfile> users) {
+        UsersAdapter adapter = new UsersAdapter(this, users, StringUtil.isStringValueEqual(currentChannel.getOwnerId(), User.getCurrentUserId()) ? addUserListener : null);
+        listView.setAdapter(adapter);
+    }
+
+    private void exit(String error) {
+        showMessage("Couldn't load channel due to " + error + ". Please try later");
+        finish();
     }
 
 }
